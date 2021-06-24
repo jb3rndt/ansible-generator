332d9b2b760ad7d4497ba93ae58ffa3c0c5fabb6
Jannis Berndt <berndtjannis@gmail.com>
2021-06-24 10:31:51 +0200

    Copy environment files to host

--- ansible-generator.py
+++ ansible-generator.py
@@ -1,8 +1,13 @@
+import os
+import re
+import pwd
 import glob
 import gzip
-import re
 from pathlib import Path
+from shutil import copyfile
 
+def get_username():
+    return pwd.getpwuid( os.getuid() )[ 0 ]
 
 class Playbook:
     name = "Deploy locally"
@@ -90,7 +95,51 @@ def removed_apt_packages():
     # zgrep -hPo '^Commandline: (apt-get|apt) install (?!.*--reinstall)\K.*' /var/log/apt/history.log{.*.gz,}
 
 
+def copy_env_files():
+    user = get_username()
+    # ~/.bashrc
+    bashrc_src = str(os.path.expanduser('~/.bashrc'))
+    bashrc_dest = f"./generated_files/env_files/{user}/.bashrc"
+    os.makedirs(os.path.dirname(bashrc_dest), exist_ok=True)
+    copyfile(bashrc_src, bashrc_dest)
+
+    # ~/.bash_aliases
+    bash_aliases_src = str(os.path.expanduser('~/.bash_aliases'))
+    bash_aliases_dest = f"./generated_files/env_files/{user}/.bash_aliases"
+    if Path(bash_aliases_src).is_file():
+        os.makedirs(os.path.dirname(bash_aliases_dest), exist_ok=True)
+        copyfile(bash_aliases_src, bash_aliases_dest)
+
+    # ~/.profile
+    profile_src = str(os.path.expanduser('~/.profile'))
+    profile_dest = f"./generated_files/env_files/{user}/.profile"
+    os.makedirs(os.path.dirname(profile_dest), exist_ok=True)
+    copyfile(profile_src, profile_dest)
+
+    # ~/.pam_environment
+    pam_src = str(os.path.expanduser('~/.pam_environment'))
+    pam_dest = f"./generated_files/env_files/{user}/.pam_environment"
+    if Path(pam_src).is_file():
+        os.makedirs(os.path.dirname(pam_dest), exist_ok=True)
+        copyfile(pam_src, pam_dest)
+    
+    # /etc/environment
+    env_src = "/etc/environment"
+    env_dest = "./generated_files/env_files/environment"
+    os.makedirs(os.path.dirname(env_dest), exist_ok=True)
+    copyfile(env_src, env_dest)
+
+    # /etc/profile.d/profile_ans.sh
+    script_src = "/etc/profile.d/profile_ans.sh"
+    script_dest = "./generated_files/env_files/profile_ans.sh"
+    if Path(pam_src).is_file():
+        os.makedirs(os.path.dirname(script_dest), exist_ok=True)
+        copyfile(script_src, script_dest)
+
+
 def main():
+    user = get_username()
+
     pb = Playbook()
     packages_to_install = installed_apt_packages()
     packages_to_remove = removed_apt_packages()
@@ -117,6 +166,39 @@ def main():
     )
     pb.tasks.append(install_packages)
     pb.tasks.append(remove_packages)
+
+    copy_env_files()
+
+    copy_user_env = Task(
+        f"Copy environment files for user {user}",
+        properties={
+            "become_user": user,
+            "template": {
+                "src": "\"{{ item.src }}\"",
+                "dest": "\"{{ item.dest }}\"",
+                "mode": "0644",
+            },
+            "loop": [
+                f'{{ src: "./env_files/{user}/.bashrc", dest: "~/.bashrc" }}',
+                f'{{ src: "./env_files/{user}/.profile", dest: "~/.profile" }}'
+            ]
+        },
+    )
+
+    copy_sys_env = Task(
+        "Copy system environment",
+        properties={
+            "ansible.builtin.copy": {
+                "src": "\"./env_files/environment\"",
+                "dest": "\"/etc/environment\"",
+                "mode": "0644",
+            }
+        }
+    )
+
+    pb.tasks.append(copy_user_env)
+    pb.tasks.append(copy_sys_env)
+
     Path("generated_files").mkdir(parents=True, exist_ok=True)
     with open("generated_files/playbook.yaml", "w") as file:
         pb.write(file)
